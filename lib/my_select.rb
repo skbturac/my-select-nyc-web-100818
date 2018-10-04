9c97ae68ccdaa43e00a479b94f6a48cdc9a7bd81
Suheyl Turac <skbsturac@live.com>
2018-10-04 17:36:12 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,22 @@
 def my_select(collection)
  # your code here!
+ 
+ items = []
+ 
+ i = 0
+ 
+  while i < collection.length
+    save = yield collection[i]
+    
+    if save == true
+      items.push(collection[i])
+      
+    end
+    
+    i += 1
+    
+  end
+  
+  items
+  
 end
